58eddee0a7f3133cf6745b1da4125f97cb653df2
samiksha <samikshapattir21@navgurukul.org>
2023-10-27 16:18:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Funct_ion from './Functional';
+
+function App() {
+  return (
+    <div className="App">
+      <Funct_ion/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Functional.js
@@ -0,0 +1,8 @@
+const Funct_ion=()=>{
+    return(
+        <>
+            <h1>Hello Maaa..!</h1>
+        </>
+    )
+}
+export default Funct_ion;
